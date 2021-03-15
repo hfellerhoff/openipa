7dfdfcf8244c92e9b0f52032495db07c36b1903e
Henry Fellerhoff <henryfellerhoff@gmail.com>
2021-03-15 04:54:26 -0500

    Fix Google Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,8 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
 
+const GA_TRACKING_ID = 'UA-159326184-1';
+
 export default class CustomDocument extends Document {
   static getInitialProps(ctx) {
     return Document.getInitialProps(ctx);
@@ -20,6 +22,26 @@ export default class CustomDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&display=swap'
             rel='stylesheet'
           />
+          {process.env.VERCEL_ENV === 'production' ? (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              ></script>
+              <script
+                async
+                dangerouslySetInnerHTML={{
+                  __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', '${GA_TRACKING_ID}');`,
+                }}
+              />
+            </>
+          ) : (
+            <></>
+          )}
         </Head>
         <body>
           <Main />
