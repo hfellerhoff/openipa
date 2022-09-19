1f1475fd0f8d571159597eb3dc17b875494ae49b
Henry Fellerhoff <henryfellerhoff@gmail.com>
2022-09-18 22:25:21 -0400

    add redirect, remove unneeded page

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/transcription',
+        destination: '/transcription/french',
+        permanent: false,
+      },
+    ];
+  },
 };
